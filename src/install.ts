14d9dfa1623dbb72464d64c1c763120cca1b25e3
Uzlopak <aras.abbasi@googlemail.com>
2022-10-18 10:35:29 +0200

    renovate project (#282)
    Use node16 for the action
    Remove uuid-dependency and use node native uuid generation
    use esbuild instead of ncc
    use latest @actions dependencies
    use pre-commit-hooks
    improve ci/cd pipeline for better confidence for code changes

--- src/install.ts
+++ src/install.ts
@@ -12,7 +12,7 @@ export type Tool = {
 
 export type Tools = Tool[]
 
-export async function downloadTool (tool: Tool): Promise<string> {
+export async function downloadTool (tool: Tool) {
   let toolDownloadPath = ''
 
   let cachedToolpath = toolCache.find(tool.name, tool.version)
@@ -37,20 +37,20 @@ export async function downloadTool (tool: Tool): Promise<string> {
   }
 
   const toolPath = path.join(cachedToolpath, tool.name)
+
   fs.chmodSync(toolPath, '777')
   // special case: copy binary to specific directory (when PATH is ignored)
   if (tool.dest !== undefined && tool.dest !== '') {
-    let result: command.Result = await command.exec('sh', ['-c', `echo "${tool.dest}"`])
-    if (!result.status) {
-      throw new Error(`cannot expand destination directory path for ${tool.name}`)
-    }
-    const destDir = result.output
-    result = await command.exec('mkdir', ['-p', `${destDir}`])
+    const baseDir = (process.env.XDG_CONFIG_HOME || (process.env.HOME || '') + '/.config')
+    // eslint-disable-next-line no-template-curly-in-string
+    const destDir = tool.dest.replace('${WAAS_TOOLS_DEST_DIR}', baseDir)
+
+    let result = await command.exec('mkdir', ['-p', `${destDir}`])
     if (!result.status) {
       throw new Error(`cannot create destination directory ${destDir} for ${tool.name}`)
     }
     const destPath = path.join(destDir, tool.name)
-    result = await command.exec('cp', ['-f', `${toolPath}`, `${destPath}`])
+    result = await command.exec('cp', ['-f', toolPath, destPath])
     if (!result.status) {
       throw new Error(`cannot copy ${tool.name} to ${destPath}`)
     }
